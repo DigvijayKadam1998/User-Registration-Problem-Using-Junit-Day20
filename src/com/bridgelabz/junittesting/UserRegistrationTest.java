34c9a5568268ad9adf991ea0895aec0b33fb1719
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-12 11:44:21 +0530

    Added: Program For Last Name Validation

--- src/com/bridgelabz/junittesting/UserRegistrationTest.java
+++ src/com/bridgelabz/junittesting/UserRegistrationTest.java
@@ -11,4 +11,10 @@ class UserRegistrationTest {
 		boolean result = userRegistration.firstName("Digvijay");
 		assertEquals(true, result);
 	}
+	
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		boolean result = userRegistration.lastName("Kadam");
+		assertEquals(true, result);
+	}
 }
